4a32a406f8232d564dc9f99a49c191bb02973c1d
JeremyMoray <etu37064@henallux.be>
2019-11-19 18:11:30 +0100

    - Modification du bouton pour revenir en arrière sur la page sign in advertiser et ré-agencement des items

--- app/src/main/java/com/example/pubeo/AdvertiserSignInActivity.java
+++ app/src/main/java/com/example/pubeo/AdvertiserSignInActivity.java
@@ -4,20 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-
-import butterknife.BindView;
 
 public class AdvertiserSignInActivity extends AppCompatActivity {
 
-    @BindView(R.id.whiteArrowSignInAdvertiser) ImageView whiteArrowSignInAdvertiser;
+    private ImageView whiteArrowSignInAdvertiser;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertiser_sign_in);
-
+        whiteArrowSignInAdvertiser = findViewById(R.id.whiteArrowSignInAdvertiser);
         whiteArrowSignInAdvertiser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
