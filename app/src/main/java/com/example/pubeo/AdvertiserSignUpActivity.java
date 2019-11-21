257ab471ccade09fb5469576f5d43402222a84a4
JeremyMoray <etu37064@henallux.be>
2019-11-21 15:11:32 +0100

    - Debut du 2ème écran d'enregistrement pour les annonceurs

--- app/src/main/java/com/example/pubeo/AdvertiserSignUpActivity.java
+++ app/src/main/java/com/example/pubeo/AdvertiserSignUpActivity.java
@@ -2,18 +2,32 @@ package com.example.pubeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AdvertiserSignUpActivity extends AppCompatActivity {
 
+    @BindView(R.id.signUpButtonAdvertiser)  Button signUpButtonAdvertiser;
     private ImageView whiteArrowSignUpAdvertiser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertiser_sign_up);
+
+        ButterKnife.bind(this);
+        signUpButtonAdvertiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openCompanyInformationsActivity();
+            }
+        });
 
         whiteArrowSignUpAdvertiser = findViewById(R.id.whiteArrowSignUpAdvertiser);
         whiteArrowSignUpAdvertiser.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +36,10 @@ public class AdvertiserSignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void openCompanyInformationsActivity(){
+        Intent intent = new Intent(this, CompanyInformationsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/pubeo/CompanyInformationsActivity.java
@@ -0,0 +1,14 @@
+package com.example.pubeo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyInformationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_informations);
+    }
+}
