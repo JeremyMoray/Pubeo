fa3f4d6174358474a2dece65736e9c52c86dcd4b
JeremyMoray <etu37064@henallux.be>
2019-12-27 14:08:40 +0100

    Ajout de l'image de profil dans les sharedPreferences

--- app/src/main/java/com/example/pubeo/Advertiser/CompanyInformationsActivity.java
+++ app/src/main/java/com/example/pubeo/Advertiser/CompanyInformationsActivity.java
@@ -1,18 +1,31 @@
 package com.example.pubeo.Advertiser;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.pubeo.R;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.provider.Telephony.Mms.Part.TEXT;
 
 public class CompanyInformationsActivity extends AppCompatActivity {
 
@@ -22,6 +35,11 @@ public class CompanyInformationsActivity extends AppCompatActivity {
     @BindView(R.id.companyPhoneField) TextInputLayout companyPhoneField;
     @BindView(R.id.companyAddressField) TextInputLayout companyAddressField;
     private ImageView whiteArrowCompanyInformations;
+    private ImageView companyLogoId;
+    private Uri imageUrl;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String IMAGEPATH = "imagePath";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +61,34 @@ public class CompanyInformationsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        companyLogoId = findViewById(R.id.companyLogoId);
+        companyLogoId.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery, String.valueOf(R.string.choosePicture)), 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            imageUrl = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUrl);
+                companyLogoId.setImageBitmap(bitmap);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void openHomeActivity(){
@@ -81,6 +127,21 @@ public class CompanyInformationsActivity extends AppCompatActivity {
         }
 
         if (isValid) {
+            companyLogoId.buildDrawingCache();
+            Bitmap bitmap = companyLogoId.getDrawingCache();
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream);
+            byte[] image=stream.toByteArray();
+            String img_str = Base64.encodeToString(image, 0);
+
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString(IMAGEPATH, img_str);
+
+            editor.apply();
+
             Intent intent = new Intent(this, HomeActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
--- app/src/main/java/com/example/pubeo/Advertiser/HomeActivity.java
+++ app/src/main/java/com/example/pubeo/Advertiser/HomeActivity.java
@@ -1,5 +1,9 @@
 package com.example.pubeo.Advertiser;
 
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.navigation.NavController;
@@ -7,6 +11,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
 import com.example.pubeo.R;
 import com.google.android.material.navigation.NavigationView;
 
@@ -15,11 +20,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String IMAGEPATH = "imagePath";
     private AppBarConfiguration mAppBarConfiguration;
+    private CircleImageView logoMenuAdvertiser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,17 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String img_str = sharedPreferences.getString(IMAGEPATH, "");
+
+        byte[] decodedString = Base64.decode(img_str, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+        View inflatedView = navigationView.getHeaderView(0);
+        logoMenuAdvertiser = inflatedView.findViewById(R.id.logoMenuAdvertiser);
+
+        Glide.with(HomeActivity.this).load(decodedByte).override(200,200).into(logoMenuAdvertiser);
     }
 
     @Override
