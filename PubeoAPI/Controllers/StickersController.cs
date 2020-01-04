394b4cb5399ec490d1d78a4d702a1a8414734674
Julien <julien.calmant@gmail.com>
2020-01-04 20:11:56 +0100

    crud sticker

--- PubeoAPI/Controllers/StickersController.cs
+++ PubeoAPI/Controllers/StickersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,7 +20,84 @@ namespace PubeoAPI.Controllers {
             _context = context;
         }
 
-        //CRUD stickers
-        
+        // GET: /Stickers
+        [HttpGet]
+        public IEnumerable<Sticker> GetStickers(){
+            return _context.Stickers;
+        }
+
+
+        // GET: /Stickers/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSticker([FromRoute] Guid id)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var sticker = await _context.Stickers.SingleOrDefaultAsync(s => s.Id == id);
+
+            if(sticker == null) return NotFound();
+
+            return Ok(sticker);
+        }
+
+        // POST: /Stickers
+        [HttpPost]
+        public async Task<IActionResult> PostSticker([FromBody] StickersDTO stickers)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sticker = await BondToProfessionnel(stickers);
+            await _context.Stickers.AddAsync(sticker);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSticker", new { id = sticker.Id }, sticker);
+        }
+
+        // DELETE: /Stickers
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSticker([FromBody] StickersDTO stickers)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var sticker = await DeleteFromBond(stickers);
+            foreach(var remSticker in sticker)
+            {
+                _context.Stickers.Remove(remSticker);
+
+            }
+            await _context.SaveChangesAsync();
+
+            return Ok(sticker);
+        }
+
+        private async Task<Sticker> BondToProfessionnel(StickersDTO stickers)
+        {
+            var stickerTemp = new Sticker();
+            if(stickers.NomEntreprise != null)
+            {
+                var pro = await _context.Professionnels
+                    .FirstOrDefaultAsync(p => p.NomEntreprise.Equals(stickers.NomEntreprise));
+                stickerTemp.ProfessionnelId = pro.Id;
+            }
+            return stickerTemp; 
+        }
+
+        private async Task<IEnumerable<Sticker>> DeleteFromBond(StickersDTO stickers)
+        {
+            var stickerTemp = new HashSet<Sticker>();
+            foreach(var sticker in _context.Stickers)
+            {
+                if(sticker.ProfessionnelId != null)
+                {
+                    var pro = await _context.Professionnels.FirstOrDefaultAsync(p => p.NomEntreprise.Equals(stickers.NomEntreprise));
+                    if(sticker.ProfessionnelId.Equals(pro.Id)) 
+                        stickerTemp.Add(sticker);
+                }
+            }
+            return stickerTemp;
+        }
     }
 }
